013bc83c53879d9639ed04883722cbb59b42eef3
Gesugao-san <gesugao.san@gmail.com>
2025-04-04 20:31:03 +0300

    Total code rewrite

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,9 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'dist',
-      assets: 'dist',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    }),
-    paths: {
-      //base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-      //base: process.env.NODE_ENV === 'production' ? '/url-shortener' : '',
-      base: process.env.NODE_ENV === 'production' ? '/' + process.env.BASE_PATH : '',
-    }
-  }
-}
+const config = {
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
+};
+
+export default config;
