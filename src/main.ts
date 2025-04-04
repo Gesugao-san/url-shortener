8b081533d33540a59381c89c3edb70087b3d3b2a
Gesugao-san <gesugao.san@gmail.com>
2025-04-04 22:26:13 +0300

    Add some improvements to code and infrastructure

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { mount } from 'svelte'
+import './app.css'
+//import './app_debug.css'
+import App from './routes/+page.svelte'
+
+// const app = mount(App, {
+//   target: document.getElementById('app')!,
+// })
+//const app = {}
+
+//export default app
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import svelteLogo from '../assets/svelte.svg'
+  import viteLogo from '../assets/vite.svg'
+  import Counter from '../lib/Counter.svelte'
+</script>
+
+<svelte:head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- <link rel="icon" type="image/svg+xml" href="/vite.svg" /> -->
+  <!-- <link rel="icon" href="%sveltekit.assets%/favicon.png" /> -->
+  <link rel="icon" href="{svelteLogo}" />
+</svelte:head>
+
+<main>
+  <div>
+    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+
+  <script type="module" src="/src/main.ts"></script>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
