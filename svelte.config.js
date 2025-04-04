b2c2644b432e9d4dc36207022c368f8199a2c327
Gesugao-san <gesugao.san@gmail.com>
2025-04-04 20:35:12 +0300

    Change config to deploy to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
@@ -12,7 +13,10 @@ const config = {
       fallback: undefined,
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
   }
 };
 
